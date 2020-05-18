179bf6befa266f2a01ac6c1f5bbcea885e271d2e
RyotaMatsushita <ryotinjpn@gmail.com>
2020-05-18 15:15:45 +0900

    Add users image column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_17_053504) do
+ActiveRecord::Schema.define(version: 2020_05_18_061220) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
